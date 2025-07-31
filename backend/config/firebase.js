a3caa6c7b8136c68054a44c4ac2ddda6d1a72f5f
HamdiNur <hamdoonur26@gmail.com>
2025-07-31 14:25:58 +0300

    Use FIREBASE_CREDENTIALS env for Firebase admin

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,6 +1,8 @@
 // 📁 config/firebase.js
 import admin from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json' assert { type: 'json' };
+// import serviceAccount from './serviceAccountKey.json' assert { type: 'json' };
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
